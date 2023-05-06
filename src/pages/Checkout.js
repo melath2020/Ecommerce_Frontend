473ef5b79b0d3e218de64a69ea0a53d82f03fe10
Aswin s <aswinsudhakaran98@gmail.com>
2023-05-07 00:01:23 +0530

    update

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,12 +4,31 @@ import { BiArrowBack } from "react-icons/bi";
 import watch from "../images/watch.jpg";
 import Container from "../components/Container";
 import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as yup from 'yup'
+import axios from "axios";
+import {config} from '../utils/axiosConfig';
+
+const shippingSchema = yup.object({
+  firstName: yup.string().required("First Name is Required"),
+  lastName:yup.string().required("Last Name is required"),
+  address:yup.string().required("Address details is required"),
+  state:yup.string().required("State is required"),
+  city:yup.string().required("City is required"),
+  country:yup.string().required("Country is required"),
+  pincode:yup.string().required("Pincode is required")
+
+});
+
 
 
 const Checkout = () => {
+
   const dispatch=useDispatch()
   const [totalAmount,setTotalAmount]=useState(null)
   const cartState=useSelector(state=>state?.auth?.cartProducts)
+  const [shippingInfo,setShippingInfo]=useState(null)
+  const [paymentInfo,setPaymentInfo]=useState({razorpayPaymentId:"",razorpayOrderId:""})
   useEffect(()=>{
     let sum =0;
     for (let index = 0; index < cartState?.length; index++) {
@@ -18,6 +37,94 @@ const Checkout = () => {
       
     }
   },[cartState])
+
+  const loadScript=(src)=>{
+    return new Promise((resolve)=>{
+      const script=document.createElement("script")
+      script.src=src;
+      script.onload=()=>{
+        resolve(true)
+      }
+      script.onerror=()=>{
+        resolve(false)
+      }
+      document.body.appendChild(script)
+    }) 
+  }
+
+  const checkOutHandler=async()=>{
+    const res=await loadScript("https://checkout.razorpay.com/v1/checkout.js")
+    if(!res){
+      alert("Razorpay SDK failed to load")
+      return;
+    }
+    const result=await axios.post("http://localhost:5000/api/user/order/checkout","",config)
+    if(!result){
+      alert("Something went wrong")
+      return;
+    }
+    const {amount,id:order_id,currency}=result.data.order
+
+    const options = {
+            key: "rzp_test_TArLTQzK283PIB", // Enter the Key ID generated from the Dashboard
+            amount: amount,
+            currency: currency,
+            name: "Aswin Sudhakar",
+            description: "Test Transaction",
+            order_id: order_id,
+            handler: async function (response) {
+                const data = {
+                    orderCreationId: order_id,
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+                    // razorpaySignature: response.razorpay_signature,
+                };
+
+                const result = await axios.post("http://localhost:5000/api/user/order/paymentVerification", data,config);
+
+               setPaymentInfo({
+               
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+               })
+            },
+            prefill: {
+                name: "Aswin",
+                email: "aswinsudhakaran98@gmail.com",
+                contact: "9999999999",
+            },
+            notes: {
+                address: "Developer's corner",
+            },
+            theme: {
+                color: "#61dafb",
+            },
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+
+  }
+  
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName:'',
+      address:'',
+      state:'',
+      city:'',
+      country:'',
+      pincode:'',
+      other:''
+    },
+    validationSchema:shippingSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values))
+      setShippingInfo(values)
+      checkOutHandler()
+     
+    },
+  });
   return (
     <>
     <Container class1="checkout-wrapper py-5 home-wrapper-2">
@@ -61,64 +168,125 @@ const Checkout = () => {
                 </p>
                 <h4 className="mb-3">Shipping Address</h4>
                 <form
+                onSubmit={formik.handleSubmit}
                   action=""
                   className="d-flex gap-15 flex-wrap justify-content-between"
                 >
                   <div className="w-100">
-                    <select name="" className="form-control form-select" id="">
+                    <select name="country" value={formik.values.country} onChange={formik.handleChange("country")} onBlur={formik.handleBlur("country")} className="form-control form-select" id="">
                       <option value="" selected disabled>
                         Select Country
                       </option>
+                      <option value="India" >
+                        India
+                      </option>
                     </select>
+                    <div className="error ms-2">
+                      {formik.touched.country && formik.errors.country}
+                    </div>
                   </div>
                   <div className="flex-grow-1">
                     <input
+                    name="firstName"
+                      value={formik.values.firstName} 
+                      onChange={formik.handleChange("firstName")} 
+                      onBlur={formik.handleBlur("firstName")}
                       type="text"
                       placeholder="First Name"
                       className="form-control"
                     />
+                     <div className="error ms-2">
+                      {formik.touched.firstName && formik.errors.firstName}
+                    </div>
                   </div>
                   <div className="flex-grow-1">
                     <input
                       type="text"
+                         name="lastName"
+                      value={formik.values.lastName} 
+                      onChange={formik.handleChange("lastName")} 
+                      onBlur={formik.handleBlur("lastName")}
                       placeholder="Last Name"
                       className="form-control"
                     />
+                    <div className="error ms-2">
+                      {formik.touched.lastName && formik.errors.lastName}
+                    </div>
                   </div>
                   <div className="w-100">
                     <input
+                    name="address"
+                    value={formik.values.address} 
+                    onChange={formik.handleChange("address")} 
+                    onBlur={formik.handleBlur("address")}
                       type="text"
                       placeholder="Address"
                       className="form-control"
                     />
+                      <div className="error ms-2">
+                      {formik.touched.address && formik.errors.address}
+                    </div>
                   </div>
                   <div className="w-100">
                     <input
                       type="text"
                       placeholder="Apartment, Suite ,etc"
                       className="form-control"
+                      name="other"
+                    value={formik.values.other} 
+                    onChange={formik.handleChange("other")} 
+                    onBlur={formik.handleBlur("other")}
                     />
+                       <div className="error ms-2">
+                      {formik.touched.other && formik.errors.other}
+                    </div>
                   </div>
                   <div className="flex-grow-1">
                     <input
                       type="text"
                       placeholder="City"
                       className="form-control"
+                      name="city"
+                      value={formik.values.city} 
+                      onChange={formik.handleChange("city")} 
+                      onBlur={formik.handleBlur("city")}
                     />
+                      <div className="error ms-2">
+                      {formik.touched.city && formik.errors.city}
+                    </div>
                   </div>
                   <div className="flex-grow-1">
-                    <select name="" className="form-control form-select" id="">
+                    <select  name="state"
+                      value={formik.values.state} 
+                      onChange={formik.handleChange("state")} 
+                      onBlur={formik.handleBlur("state")} className="form-control form-select" id="">
                       <option value="" selected disabled>
                         Select State
                       </option>
+                      <option value="Kerala" >
+                        Kerala
+                      </option>
+                      <option value="Tamzhnad" >
+                        Tamizhnad
+                      </option>
                     </select>
+                    <div className="error ms-2">
+                      {formik.touched.state && formik.errors.state}
+                    </div>
                   </div>
                   <div className="flex-grow-1">
                     <input
                       type="text"
                       placeholder="Zipcode"
                       className="form-control"
+                      name="pincode"
+                      value={formik.values.pincode} 
+                      onChange={formik.handleChange("pincode")} 
+                      onBlur={formik.handleBlur("pincode")}
                     />
+                    <div className="error ms-2">
+                      {formik.touched.pincode && formik.errors.pincode}
+                    </div>
                   </div>
                   <div className="w-100">
                     <div className="d-flex justify-content-between align-items-center">
@@ -129,6 +297,7 @@ const Checkout = () => {
                       <Link to="/cart" className="button">
                         Continue to Shipping
                       </Link>
+                      <button className="button" type="submit" onClick={checkOutHandler}>Place Order</button>
                     </div>
                   </div>
                 </form>
