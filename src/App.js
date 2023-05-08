0d6ed1462283b0e98128375c61a24a351a592eaf
Aswin s <aswinsudhakaran98@gmail.com>
2023-05-08 13:39:57 +0530

    update-private-public_Routes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ import TermAndConditions from './pages/TermAndConditions';
 import SingleProduct from './pages/SingleProduct';
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import { PrivateRoutes } from './routing/PrivateRoutes';
+import { OpenRoutes } from './routing/OpenRoutes';
 
 
 function App() {
@@ -36,13 +38,13 @@ function App() {
           <Route path='product/:id' element={<SingleProduct/>}/>
           <Route path='blogs' element={<Blog/>}/>
           <Route path='blog/:id' element={<Singleblog/>}/>
-          <Route path="cart" element={<Cart />} />
-          <Route path="checkout" element={<Checkout />} />
+          <Route path="cart" element={<PrivateRoutes><Cart /></PrivateRoutes>} />
+          <Route path="checkout" element={<PrivateRoutes><Checkout /></PrivateRoutes>} />
           <Route path='compare-product' element={<CompareProduct/>}/>
-          <Route path='wishlist' element={<Wishlist/>}/>
-          <Route path='login' element={<Login/>}/>
+          <Route path='wishlist' element={<PrivateRoutes><Wishlist/></PrivateRoutes>}/>
+          <Route path='login' element={<OpenRoutes><Login/></OpenRoutes>}/>
           <Route path='forgot-password' element={<Forgotpassword/>}/>
-          <Route path='signup' element={<Signup/>}/>
+          <Route path='signup' element={<OpenRoutes><Signup/></OpenRoutes>}/>
           <Route path='reset-password' element={<Resetpassword/>}/>
           <Route path='privacy-policy' element={<PrivacyPolicy/>}/>
           <Route path='refund-policy' element={<RefundPolicy/>}/>
